5561ec0a473b3524aab41f481eec066a4a2c97c9
Jeffery Cosby <jeffery.cosby@omes.ok.gov>
2023-01-04 11:25:23 -0600

    mongoose

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 import express from 'express';
 import routes from './src/routes/crmRoutes';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
 
 const app = express();
 const PORT = 3000;
+
+//mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/CRMdb', {
+    useNewUrlParser: true
+});
+
+//bodyParser setup
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+app.use(bodyParser.json());
+
+
 
 routes(app);
 
