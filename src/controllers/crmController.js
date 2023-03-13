7a8974b1fd290a8817dccb4e9785a7aa9205c788
Jeffery Cosby <jeffery.cosby@omes.ok.gov>
2023-03-13 10:14:25 -0500

    cleanup

--- src/controllers/crmController.js
+++ src/controllers/crmController.js
@@ -41,6 +41,8 @@ export const updateContact = (req, res) => {
     });
 };
 
+export const patchContact = (req, res) => {};
+
 export const deleteContact = (req, res) => {
     Contact.remove({ _id: req.params.contactId }, (err, contact) => {
         if (err) {
